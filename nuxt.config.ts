7d4b73b85cc39138a75d5edb1bb7f2ff73e79709
Kaya Chishiro <50231304+kaya-chi46@users.noreply.github.com>
2022-04-22 19:50:54 +0900

    ✨Font Awesomeを導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,19 @@ export default defineNuxtConfig({
   typescript: {
     strict: true,
   },
+  build: {
+    transpile: [
+      '@fortawesome/fontawesome-svg-core',
+      '@fortawesome/free-solid-svg-icons',
+      '@fortawesome/free-brands-svg-icons',
+      '@fortawesome/vue-fontawesome',
+    ],
+  },
   components: [
     {
       path: '@/components/',
       pathPrefix: false,
     },
   ],
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
 })
